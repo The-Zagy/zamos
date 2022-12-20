64add7deae73c51c57deb242931cc63437c90727
Ahmed Azzam <ahmed195138@feng.bu.edu.eg>
2022-12-20 15:50:09 +0200

    barebones fifo

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,74 @@
 import { Interval, Process, ProcessStatus, SchedulerReturn } from "./types";
-import { scene } from './scene.json'
+import { scene } from './fifo-example.json'
 import { calcResponseTime, calcTurnAroundTime } from "./utils";
 
 let processQueue: Process[];
-// function firstInFirstOut(processQueue: Process[]): SchedulerReturn {
-//     let turnAround = 0;
-//     // to keep track where we're in the time
-//     let currentTimeSlice = 0;
-//     let responseTime = 0;
-//     console.log('FIFO');
-//     // Sort array by arrival time because FIFO policy is first in first out (by arrival time)..
-//     processQueue = processQueue.sort((a, b) => (a.arrivalTime - b.arrivalTime));
-//     const intervals: Intervals = [];
-//     // calc turnAround
-//     for (const process of processQueue) {
-//         process.status = ProcessStatus.RUNNING
-//         //! log processes MONITOR 
-//         // to remove the gap if one process arrivaltime is late after the currentTimeSlice
-//         if (process.arrivalTime > currentTimeSlice) currentTimeSlice = process.arrivalTime;
-//         process.finishTime = currentTimeSlice + process.executionTime;
-//         process.firstRunTime = currentTimeSlice;
-//         currentTimeSlice += process.executionTime;
-//         intervals.push({ start: process.firstRunTime, finish: process.finishTime, pid: process.pid });
-//         console.log(calcTurnAroundTime(process), calcResponseTime(process))
-//     }
-//     return (
-//         {
-//             intervals: intervals,
-//             processesData: processQueue
-//                 .map((process => ({ pid: process.pid, responseTime: calcResponseTime(process), turnaround: calcTurnAroundTime(process) })))
-//         }
-//     )
-// }
+//
+function firstInFirstOut(processQueue: Process[]) {
+    let sortedByArrivalTime = processQueue.sort((a, b) => a.arrivalTime - b.arrivalTime);
+    let currentTime: number = 0;
+    const readyQueue: Process[] = [];
+    let blockedQueue: { process: Process, releaseTime: number }[] = [];
+    let currentlyRunning: null | Process = null;
+    let currentlyRunningTime: number = 0;
+    while (blockedQueue.length !== 0 || readyQueue.length !== 0 || sortedByArrivalTime.length !== 0 || currentlyRunning !== null) {
+
+        //find canditates to be ready from the input and the blocked queue and push them into ready queue
+        while (sortedByArrivalTime.length !== 0) {
+            const canditate = sortedByArrivalTime.findIndex((i) => {
+                if (i.arrivalTime >= currentTime) return true;
+                return false;
+            });
+            if (canditate === -1) break;
+            readyQueue.push(sortedByArrivalTime[canditate]);
+            sortedByArrivalTime.splice(canditate, 1)
+        }
+        while (blockedQueue.length !== 0) {
+            const canditate = blockedQueue.findIndex((i) => {
+                if (i.releaseTime <= currentTime) return true;
+                return false
+            })
+            if (canditate === -1) break;
+            readyQueue.push(blockedQueue[canditate].process);
+            blockedQueue.splice(canditate, 1);
+        }
+        //if nothing is running dequeue from ready queue that is the queue isn't empty
+        //if it is indeed empty just wait for the next second maybe it won't be
+        if (currentlyRunning === null) {
+            if (readyQueue.length !== 0) {
+                currentlyRunning = readyQueue.shift() as Process;
+            }
+            else {
+                currentTime++;
+                continue;
+            }
+        }
+
+        //Manipulate current running process and increment running time
+        currentlyRunning.cpuTime--;
+        currentlyRunningTime++;
+        currentTime++;
+
+        //Check if there's any i/o block if it is queue currently running into blocked queue
+        //and set currentlyRunning to null
+        if (currentlyRunning.io.find((i) => i.start === currentlyRunningTime)) {
+            const ioPeriod = currentlyRunning.io.shift();
+            blockedQueue.push({
+                process: currentlyRunning,
+                releaseTime: currentTime + ioPeriod!.length
+            })
+            currentlyRunning = null;
+            currentlyRunningTime = 0;
+        }
+        //other wise check if it's cputime is over and also set it to null
+        else if (currentlyRunning.cpuTime === 0) {
+            currentlyRunning = null;
+            currentlyRunningTime = 0;
+        }
+
+    }
+
+}
 
 // function SJF(processQueue: Process[]): SchedulerReturn {
 //     let currentTimeSlice = 0;
@@ -39,14 +76,14 @@ let processQueue: Process[];
 //     processQueue = processQueue.sort((a, b) => (a.arrivalTime - b.arrivalTime));
 //     processQueue = processQueue.sort((a, b) => {
 //         if (a !== b) return 0;
-//         return a.executionTime - b.executionTime;
+//         return a.cpuTime - b.cpuTime;
 //     })
 //     for (const process of processQueue) {
 //         if (process.arrivalTime > currentTimeSlice) currentTimeSlice = process.arrivalTime;
-//         process.finishTime = currentTimeSlice + process.executionTime;
+//         process.finishTime = currentTimeSlice + process.cpuTime;
 //         process.firstRunTime = currentTimeSlice;
 //         intervals.push({ start: process.firstRunTime, finish: process.finishTime, pid: process.pid });
-//         currentTimeSlice += process.executionTime;
+//         currentTimeSlice += process.cpuTime;
 //     }
 //     return (
 //         {
@@ -59,15 +96,15 @@ let processQueue: Process[];
 // function shortestTimeToCompletion(processQueue: Process[]): SchedulerReturn {
 //     let currentTimeSlice = 0;
 //     const intervals: Intervals = [];
-//     processQueue = processQueue.sort((a, b) => (a.executionTime - b.executionTime));
+//     processQueue = processQueue.sort((a, b) => (a.cpuTime - b.cpuTime));
 //     while (true) {
-//         const currentProcessIndex = processQueue.findIndex(process => process.arrivalTime <= currentTimeSlice && process.executionTime !== 0);
+//         const currentProcessIndex = processQueue.findIndex(process => process.arrivalTime <= currentTimeSlice && process.cpuTime !== 0);
 //         if (currentProcessIndex === -1) break;
 //         const interval: Interval = { start: currentTimeSlice, pid: processQueue[currentProcessIndex].pid, finish: 0 }
 //         if (processQueue[currentProcessIndex].firstRunTime === -1) processQueue[currentProcessIndex].firstRunTime = currentTimeSlice;
-//         processQueue[currentProcessIndex].executionTime--;
+//         processQueue[currentProcessIndex].cpuTime--;
 //         currentTimeSlice++;
-//         if (processQueue[currentProcessIndex].executionTime === 0) processQueue[currentProcessIndex].finishTime = currentTimeSlice;
+//         if (processQueue[currentProcessIndex].cpuTime === 0) processQueue[currentProcessIndex].finishTime = currentTimeSlice;
 //         interval.finish = currentTimeSlice;
 //         intervals.push(interval)
 //     }
@@ -97,7 +134,13 @@ let processQueue: Process[];
 //     });
 // }
 //Round Robin, MLFQ
+function checkIntegrityOfInput(input: Process[]) {
+    for (const process of input) {
+        //check that i/o is within execution time
+    }
+}
 function main() {
     processQueue = (scene as unknown) as Process[];
+    firstInFirstOut(processQueue);
 }
 main();
