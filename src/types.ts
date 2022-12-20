f5055504a801162216d3ab2b1cef26a8a2cb5541
Ahmed Azzam <ahmed195138@feng.bu.edu.eg>
2022-12-20 09:28:47 +0200

    type change

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import { type } from "os";
+import { start } from "repl";
+
 export enum ProcessStatus {
     RUNNING = 'running',
     READY = 'ready',
@@ -5,6 +8,7 @@ export enum ProcessStatus {
 
 }
 export interface Process {
+    name: string,
     pid: number;
     arrivalTime: number;
     finishTime: number;
@@ -13,14 +17,34 @@ export interface Process {
     status: ProcessStatus
 }
 export type ProcessFinalStats = {
+    name: string
+    pid: number,
     turnaround: number,
     responseTime: number,
     finishTime: number,
-    pid: number,
+    interval: Interval
 }
 export type ProcessesFinalStats = Array<ProcessFinalStats>
-export type Intervals = [{ [k: string]: { start: number, finish: number, status: ProcessStatus }[] }]
-export interface SchedulerReturn {
-    processesData: ProcessesFinalStats,
-    intervals: Intervals
-}
+export type Interval = { start: number, finish: number, status: ProcessStatus }[]
+
+export type SchedulerReturn = ProcessesFinalStats
+
+//example
+const ret: SchedulerReturn = [
+    {
+        name: "A",
+        pid: 3,
+        finishTime: 5,
+        responseTime: 23,
+        turnaround: 32,
+        interval: [{ start: 1, finish: 4, status: ProcessStatus.READY }, { start: 4, finish: 7, status: ProcessStatus.BLOCKED }]
+    },
+    {
+        name: "B",
+        pid: 1,
+        finishTime: 2,
+        responseTime: 13,
+        turnaround: 6,
+        interval: [{ start: 9, finish: 15, status: ProcessStatus.RUNNING }, { start: 4, finish: 7, status: ProcessStatus.BLOCKED }]
+    }
+]
